95e22b9863942706b26a47e29c95df1e56aa41a3
kingbj940429 <kingbj0429@gmail.com>
2020-07-05 14:02:01 +0900

    Update files

--- app.js
+++ app.js
@@ -3,21 +3,35 @@ var http = require('http');
 var path = require('path');
 var static = require('serve-static');
 
+
 var app = express();
 
 app.use(static(path.join(__dirname, '/')));
 
 app.set('port', process.env.PORT || 3000);
-app.get('/search',function(req,res){
-    next();
+app.get('/',function(req,res){
+
 });
 
 app.use(function(req,res,next){
 
     var input_text = req.query.search;
     console.log(input_text + "을 검색하셨습니다.");
-    res.redirect("/result.html");
+    res.redirect("/result.html?search="+input_text);
+    next();
 });
+app.use(function(req,res,next){
+    console.log("다음");
+    next();
+})
+app.use(function(req,res,next){
+    var result_text = req.query.search;
+    console.log(result_text + "을 검색하셨습니다.");
+    
+});
+
+
+
 
 
 //서버 
