2022822b79ce8024aacc5888fd17a4e83650b952
kingbj940429 <kingbj0429@gmail.com>
2020-07-05 18:31:40 +0900

    Change Kakao searh box

--- js/selectImage.js
+++ js/selectImage.js
@@ -1,3 +1,4 @@
+var food_name;
 if(input_text[1]=="단맛"){
     document.getElementById('picture').src=logo_info()
         function logo_info() {
@@ -5,6 +6,7 @@ if(input_text[1]=="단맛"){
             img_src = './images/chocolate.jpg';
         return img_src;
         }
+    food_name = "초콜릿";
 
 }else if(input_text[1]=="신맛"){
     document.getElementById('picture').src=logo_info()
@@ -13,4 +15,5 @@ if(input_text[1]=="단맛"){
         img_src = './images/orange.jpg';
     return img_src;
     }
+    food_name = "오렌지";
 }
